b9afcee59fd6dd1a3bad5978d04d2c9685d3d62a
Spyridon Alfredos Desyllas <sadesyllas@gmail.com>
2020-01-29 17:39:16 +0200

    wip

---
+++ src/Renci.SshNet.New.App/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Renci.SshNet.New.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListFiles(args[0], args[1], args[2]);
+        }
+
+        private static void ListFiles(string host, string username, string password)
+        {
+            string remoteDirectory = ".";
+
+            using (SftpClient sftp = new SftpClient(host, username, password))
+            {
+                try
+                {
+                    sftp.Connect();
+
+                    var files = sftp.ListDirectory(remoteDirectory);
+
+                    foreach (var file in files)
+                    {
+                        Console.WriteLine(file.Name);
+                    }
+
+                    sftp.Disconnect();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An exception has been caught " + e.ToString());
+                }
+            }
+        }
+    }
+}
